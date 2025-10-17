d6e03b5a4fb5bbf263f626255c4abc8e180f4865
rahilmhd <rahilmuhammed115@gmail.com>
2025-10-17 11:55:49 +0530

    Add files via upload

---
+++ area.py.py
@@ -0,0 +1,11 @@
+# Area of square: side * side
+area_square = lambda side: side * side
+
+# Area of rectangle: length * width
+area_rectangle = lambda length, width: length * width
+
+# Area of triangle: 0.5 * base * height
+area_triangle = lambda base, height: 0.5 * base * height
+print(area_square(4))        
+print(area_rectangle(5, 3))  
+print(area_triangle(6, 2))  
---
+++ ing.py
@@ -0,0 +1,8 @@
+string=input("enter the string to be checked")
+
+print(len(string))
+pos=(len(string)-3)
+
+print(string[-1:len(string)-3])
+
+if(string[pos:-1])=="ing"):
